94df3d9d7d96d137a363af1744cad684f710ff79
wangzaijun <wzj@simpletour.com>
2016-10-20 11:23:29 +0800

    LongestPalindromicSubstring

--- src/com/leetcode/code/LongestPalindromicSubstring.java
+++ src/com/leetcode/code/LongestPalindromicSubstring.java
@@ -5,6 +5,8 @@ package com.leetcode.code;
  *
  * Given a string S, find the longest palindromic substring in S.
  * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ *
+ * 一个字符 也算回文
  */
 public class LongestPalindromicSubstring {
 
@@ -14,13 +16,40 @@ public class LongestPalindromicSubstring {
 
     public String longestPalindrome(String s) {
 
+        String palindrome = "";
+        if (s.length()<=1){
+            return s;
+        }
+        for (int i = 0;i < s.length(); i++){
+            for (int j = i+1; j < s.length();j++){
+                String tempString = s.substring(i,j);
+                String reverseCompareString = this.reverseString(tempString);
+                if (reverseCompareString.length() <= (s.length() - j)){
+                    String compareString = s.substring(j,reverseCompareString.length());
+                    if (compareString.equals(reverseCompareString) && compareString.length()*2 > palindrome.length()){
+                        palindrome = tempString + reverseCompareString;
+                    }
+                }
+            }
+        }
 
+        return palindrome;
 
-        return "";
     }
 
+    /**
+     * 反转之后的字符串
+     * @param s
+     * @return
+     */
     public String reverseString(String s){
-        return "";
+        if (s.isEmpty() || s.length() <= 1) return s;
+        char[] chars = s.toCharArray();
+        String result = "";
+        for (int i = s.length() -1 ; i >=0 ; i--){
+            result = result + chars[i];
+        }
+        return result;
     }
 
 //    public class stringStack{
