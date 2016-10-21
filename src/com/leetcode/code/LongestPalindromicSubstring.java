62f006f52323908c8059c52066248a605e9f18be
wangzaijun <wzj@simpletour.com>
2016-10-21 10:48:02 +0800

    LongestPalindromicSubstring  166ms 版本

--- src/com/leetcode/code/LongestPalindromicSubstring.java
+++ src/com/leetcode/code/LongestPalindromicSubstring.java
@@ -19,8 +19,13 @@ public class LongestPalindromicSubstring {
         Long start = System.currentTimeMillis();
         String ss = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
 //        String ss = "jglknendplocymmvwtoxvebkekzfdhykknufqdkntnqvgfbahsljkobhbxkvyictzkqjqydczuxjkgecdyhixdttxfqmgksrkyvopwprsgoszftuhawflzjyuyrujrxluhzjvbflxgcovilthvuihzttzithnsqbdxtafxrfrblulsakrahulwthhbjcslceewxfxtavljpimaqqlcbrdgtgjryjytgxljxtravwdlnrrauxplempnbfeusgtqzjtzshwieutxdytlrrqvyemlyzolhbkzhyfyttevqnfvmpqjngcnazmaagwihxrhmcibyfkccyrqwnzlzqeuenhwlzhbxqxerfifzncimwqsfatudjihtumrtjtggzleovihifxufvwqeimbxvzlxwcsknksogsbwwdlwulnetdysvsfkonggeedtshxqkgbhoscjgpiel";
+//        String ss = "anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgxkzdbcjsjjovnhpnbkurxqfupiprpbiwqdnwaqvjbqoaqzkqgdxkfczdkznqxvupdmnyiidqpnbvgjraszbvvztpapxmomnghfaywkzlrupvjpcvascgvstqmvuveiiixjmdofdwyvhgkydrnfuojhzulhobyhtsxmcovwmamjwljioevhafdlpjpmqstguqhrhvsdvinphejfbdvrvabthpyyphyqharjvzriosrdnwmaxtgriivdqlmugtagvsoylqfwhjpmjxcysfujdvcqovxabjdbvyvembfpahvyoybdhweikcgnzrdqlzusgoobysfmlzifwjzlazuepimhbgkrfimmemhayxeqxynewcnynmgyjcwrpqnayvxoebgyjusppfpsfeonfwnbsdonucaipoafavmlrrlplnnbsaghbawooabsjndqnvruuwvllpvvhuepmqtprgktnwxmflmmbifbbsfthbeafseqrgwnwjxkkcqgbucwusjdipxuekanzwimuizqynaxrvicyzjhulqjshtsqswehnozehmbsdmacciflcgsrlyhjukpvosptmsjfteoimtewkrivdllqiotvtrubgkfcacvgqzxjmhmmqlikrtfrurltgtcreafcgisjpvasiwmhcofqkcteudgjoqqmtucnwcocsoiqtfuoazxdayricnmwcg";
+
+//        String ss = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir";
+
         // ranynar
         String result = palindromicSubstring.longestPalindrome(ss);
+
         Long end = System.currentTimeMillis();
         System.out.println("所用时间： " + (end-start) + "ms");
         System.out.println("回文: "+result);
@@ -57,99 +62,23 @@ public class LongestPalindromicSubstring {
             }
         }
         return palindrome;
-
     }
-
     /**
      * 判断字符串是不是回文
      * @param s
      * @return
      */
     public Boolean isPalindromeString(String s){
-        Long start = System.currentTimeMillis();
         int length = s.length();
-        int halfLength = length/2;
+        char[] charS = s.toCharArray();
 
         if (length<2) return Boolean.TRUE;
-        if (length%2 == 0){
-            String preString = s.substring(0,halfLength);
-            String postString = s.substring(halfLength,length);
-//            if (this.reverseString(preString).equals(postString)){
-            if (this.compareString(preString,postString)){
-
-                Long endTime = System.currentTimeMillis();
-                time = time + (endTime-start);
-                return Boolean.TRUE;
-            }else {
-                Long endTime = System.currentTimeMillis();
-                time = time + (endTime-start);
-                return Boolean.FALSE;
-            }
-        }else {
-            String preString = s.substring(0,halfLength);
-            String postString = s.substring(halfLength+1,length);
-//            if (this.reverseString(preString).equals(postString)){
-            if (this.compareString(preString,postString)){
-
-                Long endTime = System.currentTimeMillis();
-                time = time + (endTime-start);
-                return Boolean.TRUE;
-            }else {
-                Long endTime = System.currentTimeMillis();
-                time = time + (endTime-start);
-                return Boolean.FALSE;
-            }
-        }
-    }
-
-    public Boolean compareString(String stringA, String stringB){
-        int length = stringA.length();
-        if (length != stringB.length()) return Boolean.FALSE;
-        char[] charA = stringA.toCharArray();
-        char[] charB = stringB.toCharArray();
-        for (int i =0,j=length-1;   i<length ||j >=0;  i++,j--){
-            if (charA[i] != charB[j]){
+        for (int i =0,j=length-1;   i<length || j >= 0;  i++,j--) {
+            if (i >= j) break;
+            if (charS[i] != charS[j]) {
                 return Boolean.FALSE;
             }
         }
         return Boolean.TRUE;
     }
-
-
-    /**
-     * 反转字符串
-     * @param s
-     * @return
-     */
-    public String reverseString(String s){
-        Long start = System.currentTimeMillis();
-        // method 1
-//        if (s.isEmpty() || s.length() <= 1) return s;
-//        char[] chars = s.toCharArray();
-//        StringBuilder result = new StringBuilder();
-//        for (int i = s.length() -1 ; i >= 0 ; i--){
-//            result = result .append(chars[i]);
-//        }
-        // method 2
-        String result = new StringBuilder(s).reverse().toString();
-
-        //method 3
-//        char[] str = s.toCharArray();
-//
-//        int begin = 0;
-//        int end = s.length() - 1;
-//
-//        while (begin < end) {
-//            str[begin] = (char) (str[begin] ^ str[end]);
-//            str[end] = (char) (str[begin] ^ str[end]);
-//            str[begin] = (char) (str[end] ^ str[begin]);
-//            begin++;
-//            end--;
-//        }
-        Long endTime = System.currentTimeMillis();
-        time = time + (endTime-start);
-//        return new String(str);
-        return result;
-    }
-
 }
